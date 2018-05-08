6a07abb9ad7457d27904e53321c8f98b3c7d6660
liwei0027 <liwei@qfpay.com>
2018-05-08 18:59:48 +0800

    cd

--- case/test_case.py
+++ case/test_case.py
@@ -2,3 +2,20 @@
 import unittest
 import threading
 
+class CaseTeat(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print "this is class"
+    def setUp(self):#自带
+        print "this is setup"
+    def test_01(self):
+        print "this is case"
+    def test_02(self):
+        print "this is case 2"
+    def tearDown(self):
+        print "this is teardown"
+    @classmethod
+    def tearDownClass(cls):
+        print "this is class teardaom"
+if __name__=='__main__':
+    unittest.main()
